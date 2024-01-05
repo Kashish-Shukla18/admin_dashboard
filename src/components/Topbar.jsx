ebb8c688100c3bd73b96cb00751f04087c0c593b
Kashish Shukla <86675818+Kashish-Shukla18@users.noreply.github.com>
2024-01-05 08:58:59 +0000

    First Commit

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div className='topbar'> 
+    <div className='topbarWrapper'>
+        <div className='topLeft'>
+            <span>
+                Admin Dashboard
+            </span>
+        </div>
+        <div className='topRight'>
+            <div className='topbarIcons'>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Topbar
